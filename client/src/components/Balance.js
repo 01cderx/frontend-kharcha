913f4a63e3770a7c8d7b8a9fdf76a0cd0a66af37
Divyansh <divyanshnigam12345678@gmail.com>
2025-06-05 12:22:27 +0530

    Update Balance.js

--- client/src/components/Balance.js
+++ client/src/components/Balance.js
@@ -5,7 +5,9 @@ import  numberWithCommas  from '../utils/format'
 const Balance = () => {
   const {transactions} = useContext(GlobalContext)
 
-  const amounts = transactions.map(transaction => transaction.amount)
+ const amounts = Array.isArray(transactions)
+    ? transactions.map(transaction => transaction.amount)
+    : [];
   const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2) // toFixed(2) is used to round the number to 2 decimal places
   return (
     <>
